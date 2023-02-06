db2d298e0a432216377dccae747b8a6dc6331fba
millaker <lovetaeyeon507@gmail.com>
2023-02-06 19:41:02 +0800

    Assignment 4 part 1 done

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,6 +15,28 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data *data = (struct thread_data*) thread_param;
+    struct timespec time_obtain;
+    struct timespec time_release;
+    time_obtain.tv_sec = 0;
+    time_obtain.tv_nsec = data->wait_to_obtain_ms * 1000000;
+    time_release.tv_sec = 0;
+    time_release.tv_nsec = data->wait_to_release_ms * 1000000;
+
+    nanosleep(&time_obtain, NULL);
+
+    if(pthread_mutex_lock(data->mutex)){
+        data->thread_complete_success = false;
+        return thread_param;
+    }
+
+    nanosleep(&time_release, NULL);
+
+    if(pthread_mutex_unlock(data->mutex)){
+        data->thread_complete_success = false;
+        return thread_param;
+    }
+    data->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +51,17 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* data = malloc(sizeof(struct thread_data));
+    if(!data)
+        return false;
+    // Initialize thread_data 
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->mutex = mutex;
+    data->thread_complete_success = false;
+    // Create new thread
+    if (pthread_create(thread, NULL, threadfunc, data))
+        return false;
+    return true;
 }
 
